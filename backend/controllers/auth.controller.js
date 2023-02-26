602beab962b4c72397ed7b59f8f3038df71a8767
geektousif <50136079+geektousif@users.noreply.github.com>
2023-02-26 07:47:31 +0000

    authcontroller - reset password

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -2,6 +2,7 @@ import User from "../models/user.schema";
 import asyncHandler from "../services/asyncHandler";
 import CustomError from "../utils/customError";
 import mailHelper from "../utils/mailHelper";
+import crypto from "crypto";
 
 export const cookieOptions = {
   expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
@@ -59,7 +60,7 @@ export const login = asyncHandler(async (req, res) => {
     throw new CustomError("Please fill all fields", 400);
   }
 
-  const user = User.findOne({ email }).select("+password");
+  const user = await User.findOne({ email }).select("+password");
 
   if (!user) {
     throw new CustomError("Invalid Credentials", 400);
@@ -150,3 +151,52 @@ export const forgotPassword = asyncHandler(async (req, res) => {
   }
 });
 
+/**************************************
+ * @RESET_PASSWORD
+ * @route http://localhost:4000/api/auth/password/reset/:resetToken
+ * @description User will be able to reset password based on url token
+ * @parameters token from url, password, confirm password
+ * @return User Object
+ ****************************************/
+export const resetPassword = asyncHandler(async (req, res) => {
+  const { token: resetToken } = req.params;
+  const { password, confirmPassword } = req.body;
+
+  const resetPasswordToken = crypto
+    .createHash("sha256")
+    .update(resetToken)
+    .digest("hex");
+
+  const user = await User.findOne({
+    forgotPasswordToken: resetPasswordToken,
+    forgotPassword: { $gt: Date.now() },
+  });
+
+  if (!user) {
+    throw new CustomError("password token is invalid/expired", 400);
+  }
+
+  if (password !== confirmPassword) {
+    throw new Error("password and confirm password doesn't match", 400);
+  }
+
+  user.password = password;
+  user.forgotPasswordToken = undefined;
+  user.forgotPasswordExpiry = undefined;
+
+  await user.save();
+
+  // token create
+  const token = user.getJwtToken();
+  user.password = undefined;
+
+  // TODO: helper method for cookie can be added
+
+  res.cookie("token", token, cookieOptions);
+  res.status(200).json({
+    success: true,
+    user,
+  });
+});
+
+// TODO: create controller for change password
