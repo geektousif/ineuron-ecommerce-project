6dd14e649e7f89c307654dd7e9b463db499a5501
geektousif <50136079+geektousif@users.noreply.github.com>
2023-02-26 05:34:51 +0000

    added 3rd party middlewares

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,16 @@
 import express from "express";
+import cookeParser from "cookie-parser";
+import cors from "cors";
+import morgan from "morgan";
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(cookeParser());
+
+// morgan logger
+app.use(morgan("tiny"));
 
 export default app;
